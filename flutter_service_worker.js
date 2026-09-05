e7fb046a8d8a55a1c22c4644f62bee5d69f95a51
ferrarapeppe <ferrarapeppe@users.noreply.github.com>
2026-09-05 16:29:14 +0000

    deploy: c7fab73a7ce6240d78505cc95c6a2ed0302e7364

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"manifest.json": "72a6bcf5b6ec23ec7e1770d1cc811e22",
 "version.json": "a2e7a9ec71e09383f1d1ce4b1f41976f",
-"main.dart.js": "1fd058aa09b0b7aa342c98b77ade496c",
-"flutter_bootstrap.js": "3eab5f08ef0ba127a645c882deb980d4",
+"main.dart.js": "4b08cc051db6f776734e43586fd860b2",
+"flutter_bootstrap.js": "cb1e322f7f819544a3aca1f130ebadbd",
 "icons/Icon-maskable-512.png": "cfcf15485e8268426f3bc5da803de4a9",
 "icons/Icon-192.png": "b1453d18cf70a811056a6a28fb496ecf",
 "icons/Icon-maskable-192.png": "b1453d18cf70a811056a6a28fb496ecf",
